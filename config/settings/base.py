fd3c9fb50ea7c1981d00dd7ca29b5577866cef33
Cervin-van <chervinivan@gmail.com>
2026-09-25 11:10:10 +0300

    chore: use English for API messages

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,7 +75,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = "uk"
+LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Europe/Kyiv"
 USE_I18N = True
 USE_TZ = True
